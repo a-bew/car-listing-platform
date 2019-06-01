b386baabc954dc28960b6c58a510aad4621a8c0e
Adetola Bewaji <adetola013@gmail.com>
2019-06-01 12:02:56 +0100

    added two new function: 'makeReadable()' and 'isEven()' to 'indexPage()'

--- public/js/index.js
+++ public/js/index.js
@@ -4,19 +4,35 @@ function utilityFunction(){
     populateApp(indexPage, "http://localhost:3000/cars")
 }
 
+var makeReadable = function( readable ){
+  return readable.replace("--", " ")
+}
 var indexPage = function( data ) {
     // Empty content string
     var tableContent = '';
     userListData = data;
-
+    var dataLength = data.length
+    var isEven = function(){
+    	return dataLength%2 == 0
+    }
     // For each item in our JSON, add a table row and cells to the content string
-    data.forEach(dataObj => {      
+    index = 0
+    while(index<data.length) {     
+      dataObj=data[index]
+
+      if (!isEven()){
+         if (index == dataLength-1){
+            console.log(index)
+         	break;
+         }
+      } 
       // Stick our user data array into a userlist variable in the global object
       
       tableContent += '<article class="left">';
       tableContent += '<img src="'+ dataObj.img + '">';
-      tableContent += '<p><span>' +dataObj.name+ '</span>'+ " N"+ dataObj.Price+ '</p></article>'
-    });
+      tableContent += '<p><span>' +makeReadable(dataObj.name)+ '</span>'+ " N"+ makeReadable(dataObj.Price)+ '</p></article>'
+      index++;
+    };
     
     // Inject the whole content string into our existing HTML table
     console.log("ldero", tableContent);
